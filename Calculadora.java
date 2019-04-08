334f2ed0454fe8996ebd6c70dfdbc3e87e1f670e
Josh6456 <angelopaulojunior@gmail.com>
2019-04-07 22:03:00 -0300

    Adicao

--- Calculadora.java
+++ Calculadora.java
@@ -40,3 +40,36 @@ public class Calculadora1 extends JFrame {
     }
 
 }
+
+public Calculadora() {
+        setBackground(Color.MAGENTA);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 430, 200);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.PINK);
+        contentPane.setForeground(Color.CYAN);
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel lblCalculadoraEngenharia = new JLabel("Calculadora para a professora");
+        lblCalculadoraEngenharia.setFont(new Font("Microsoft JhengHei Light", Font.PLAIN, 25));
+        lblCalculadoraEngenharia.setBounds(35, 10, 351, 40);
+        contentPane.add(lblCalculadoraEngenharia);
+
+        tfNumero1 = new JTextField();
+        tfNumero1.setBounds(5, 100, 50, 40);
+        contentPane.add(tfNumero1);
+        tfNumero1.setColumns(10);
+
+        tfNumero2 = new JTextField();
+        tfNumero2.setColumns(10);
+        tfNumero2.setBounds(280, 100, 50, 40);
+        contentPane.add(tfNumero2);
+
+        JButton btSoma = new JButton("+");
+        btSoma.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Soma();
+            }
+        });

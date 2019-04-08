0e9b1f88f09ac3a2072048298d3233514f3d7693
igorzciota <igorzornitta0805@gmail.com>
2019-04-07 22:25:16 -0300

    alteracoes

--- Calculadora.java
+++ Calculadora.java
@@ -6,74 +6,74 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
 
-public class Calculadora1 extends JFrame {
 
-    private JPanel contentPane;
+public class Calculadora extends JFrame {
 
-    /
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Calculadora1 frame = new Calculadora1();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	private JPanel contentPane;
+	private JTextField tfNumero1;
+	private JTextField tfNumero2;
+	private JTextField tfResultado;
 
-    /
-     * Create the frame.
-     */
-    public Calculadora1() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculadora frame = new Calculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-}
-
-public Calculadora() {
-        setBackground(Color.MAGENTA);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 430, 200);
-        contentPane = new JPanel();
-        contentPane.setBackground(Color.PINK);
-        contentPane.setForeground(Color.CYAN);
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
-
-        JLabel lblCalculadoraEngenharia = new JLabel("Calculadora para a professora");
-        lblCalculadoraEngenharia.setFont(new Font("Microsoft JhengHei Light", Font.PLAIN, 25));
-        lblCalculadoraEngenharia.setBounds(35, 10, 351, 40);
-        contentPane.add(lblCalculadoraEngenharia);
-
-        tfNumero1 = new JTextField();
-        tfNumero1.setBounds(5, 100, 50, 40);
-        contentPane.add(tfNumero1);
-        tfNumero1.setColumns(10);
-
-        tfNumero2 = new JTextField();
-        tfNumero2.setColumns(10);
-        tfNumero2.setBounds(280, 100, 50, 40);
-        contentPane.add(tfNumero2);
-
-        JButton btSoma = new JButton("+");
-        btSoma.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Soma();
-            }
-        });
-	btSoma.setBounds(60, 100, 50, 40);
+	/**
+	 * Create the frame.
+	 */
+	public Calculadora() {
+		setBackground(Color.MAGENTA);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 430, 200);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.PINK);
+		contentPane.setForeground(Color.CYAN);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCalculadoraEngenharia = new JLabel("Calculadora para a professora");
+		lblCalculadoraEngenharia.setFont(new Font("Microsoft JhengHei Light", Font.PLAIN, 25));
+		lblCalculadoraEngenharia.setBounds(35, 10, 351, 40);
+		contentPane.add(lblCalculadoraEngenharia);
+		
+		tfNumero1 = new JTextField();
+		tfNumero1.setBounds(5, 100, 50, 40);
+		contentPane.add(tfNumero1);
+		tfNumero1.setColumns(10);
+		
+		tfNumero2 = new JTextField();
+		tfNumero2.setColumns(10);
+		tfNumero2.setBounds(280, 100, 50, 40);
+		contentPane.add(tfNumero2);
+		
+		JButton btSoma = new JButton("+");
+		btSoma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Soma();
+			}
+		});
+		btSoma.setBounds(60, 100, 50, 40);
 		contentPane.add(btSoma);
 		
 		JButton btSub = new JButton("-");
@@ -120,17 +120,17 @@ public Calculadora() {
 		
 		
 
-		}
+	}
 	private void Soma() {
-       		tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) + Integer.parseInt(tfNumero2.getText())));
-   	 }
-    	private void Subtracao() {
-        	tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) - Integer.parseInt(tfNumero2.getText())));
-   	 }
-   	 private void Multiplicacao() {
-        	tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) * Integer.parseInt(tfNumero2.getText())));
-   	 }
-    	private void Divisao(){
-        	tfResultado.setText(Double.toString(Double.parseDouble(tfNumero1.getText()) / Double.parseDouble(tfNumero2.getText())));
-    	}
+		tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) + Integer.parseInt(tfNumero2.getText())));
+	}
+	private void Subtracao() {
+		tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) - Integer.parseInt(tfNumero2.getText())));
+	}
+	private void Multiplicacao() {
+		tfResultado.setText(Integer.toString(Integer.parseInt(tfNumero1.getText()) * Integer.parseInt(tfNumero2.getText())));
+	}
+	private void Divisao(){
+		tfResultado.setText(Double.toString(Double.parseDouble(tfNumero1.getText()) / Double.parseDouble(tfNumero2.getText())));
+	}
 }
